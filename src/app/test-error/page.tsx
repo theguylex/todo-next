7b256fac665a8b6e48b6d0a4922e59d43d9b866b
Chinonye Ekpendu <alexekpendu001@gmail.com>
2025-09-27 12:31:31 +0100

    env.node env

--- src/app/test-error/page.tsx
+++ src/app/test-error/page.tsx
@@ -1,6 +1,10 @@
-export const dynamic = 'force-dynamic';
 import type { ReactElement } from 'react';
 
+export const dynamic = 'force-dynamic';
+
 export default function TestError(): ReactElement {
+  if (process.env.NODE_ENV !== 'development') {
+    return <div>Test Error Page</div>;
+  }
   throw new Error('Oops! This is a test error.');
 }
