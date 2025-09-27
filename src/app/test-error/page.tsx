abea781911d3bcd9afa4647eea05bdfa4f775058
Chinonye Ekpendu <alexekpendu001@gmail.com>
2025-09-27 12:14:12 +0100

    new error rerender on build

--- src/app/test-error/page.tsx
+++ src/app/test-error/page.tsx
@@ -1,8 +1,5 @@
-import type { ReactElement } from "react";
+// import type { ReactElement } from 'react';
 
-export default function TestError(): ReactElement {
-  if (process.env.NODE_ENV === "development") {
-    throw new Error("Oops! This is a test error.");
-  }
-  return <div>Test Error Page</div>;
-}
+// export default function TestError(): ReactElement {
+//   throw new Error('Oops! This is a test error.');
+// }
